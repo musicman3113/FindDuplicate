282e0c5fb526adf32d9988f256b5191e4fe1b611
Tim Lonergan <musicman3113@yahoo.com>
2020-12-03 10:23:13 -0600

    Add files via upload

---
+++ Function.ps1
@@ -0,0 +1,46 @@
+﻿function Find_Duplicate([STRING] $MyString)
+{
+
+
+
+
+
+    ##Write-Host $MyString 
+
+
+    $Found=@()
+
+
+    For ($num=0 ;$num -lt $MyString.Length  ;$num++)
+    {
+
+        ##"Letter Position $num = " + $MyString.Substring($num, 1)
+
+        ##$Found = $Found + $MyString.Substring($num, 1)
+
+        ##Write-Host "Found = " $Found
+
+        ##Write-Host $Found.IndexOf($MyString.Substring($num, 1))
+
+    
+
+        if ($Found.IndexOf($MyString.Substring($num, 1)) -ge 0)
+        {
+            Return $MyString.Substring($num, 1)
+
+
+        } 
+
+    $Found = $Found + $MyString.Substring($num, 1)
+
+    }
+
+
+
+            Return "No Duplicates Found."
+
+
+
+
+
+}
